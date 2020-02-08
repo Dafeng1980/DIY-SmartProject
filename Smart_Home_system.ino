5791897e55b849b8941cb0f4bdf3d52e9c23087f
Dafeng <dafeng.li@outlook.com>
2020-02-09 00:04:05 +0800

    update

--- Smart_Home_system.ino
+++ Smart_Home_system.ino
@@ -39,103 +39,99 @@ void setup() {
     pinMode(kPowerSavePin, OUTPUT);  
       pinMode(kAirConditionPushPin, OUTPUT);
         pinMode(kAirConditionPullPin, OUTPUT);
-    portinit();
+          portinit();
   
       pinMode(kLightSensorLPin, INPUT);
         pinMode(kLightSensorHPin, INPUT);
           pinMode(kTemperaturePin, INPUT);
             pinMode(kBatteryPin, INPUT);
-              analogReference(INTERNAL);    //基准2.63V
+              analogReference(INTERNAL);    //实际基准2.63V
 
       myservo.attach(kServoPin);
-        EEPROM.get(0x10, pos);
-          if(pos >90 && pos < 1)
-          {
-            pos = 3;
-            EEPROM.put(0x10, pos);
-          }
-            myservo.write(pos);
-  
-    EEPROM.get(0x00,cycle);
-        if ( cycle > 900 ){
-          cycle = 0;
-          EEPROM.put(0x00, cycle);
-        }
-  Serial.begin(115200);
-    Serial.println("  Reset_Print_Test: ");
-    delay(100);
-      homestatus = 0;
-        posstatus = true;
-}
+        eeprominit();
+          myservo.write(pos);
+            Serial.begin(115200);
+                  homestatus = 0;
+                    posstatus = true;
+    }
 
 void loop() {    
     digitalWrite(kPowerSavePin, HIGH);
       getallval();
         cycle++;
-          Serial.print("POS_STATUS: ");
-          Serial.println(posstatus);
-          delay(200);
-            if (posstatus)  detectsensors(); 
-              Serial.print("=============================CYCLE: ");
-              Serial.println(cycle);
-                delay(300);
-              Serial.print("===HOMESTATUS: ");
-              Serial.println(homestatus);
-                delay(300);
-        detectpower();
-    switch (homestatus){
-      case 0:                     //状态0， 正常状态，自动检测各种信号，和每隔一段时间调整太阳能板位置。运行10分钟后关机。
-          if (cycle >= 101){         
-              cycle = 0;
-              EEPROM.put(0x00, cycle);
-              powerdown();
+			if (posstatus)  detectsensors();
+     
+      detectpower(); 
+        switch (homestatus){
+          case 0:                         //状态0， 正常状态，完成自动检测各种信号后自动待机1.9秒，
+              if (cycle >= 201){          //       和每隔3分钟时间调整太阳能板位置。待机6分钟后关机。
+                    cycle = 0;
+                      EEPROM.put(0x00, cycle);
+                        powerdown();               //待机约6分钟后，关机
+                  }
+    
+        	    if (cycle == 1 || !(cycle % 100)) {    
+        		        posstatus = false;
+        		          homestatus = 1;
+        	      }	  
+                  else {
+                          EEPROM.put(0x00, cycle);
+                              powerstandby();       //检测完成后待机1.9秒
+                    }
+        			break;
+          
+          case 1:                            //状态1，自动调整太阳能板位置一次
+      		    setpos();
+      		      posstatus = true;
+      		        homestatus = 0;
+      			        break;
+          
+    	    case 2:                           //状态2，任何时候，低于光感应值门限或不在舒适温度范围（温度感应值低于或高于门限）；
+    		    if (cycle >= 720) {             //      和每隔6分钟调整太阳能板位置，工作12分种后关机。
+    			      cycle = 0;
+    			        EEPROM.put(0x00, cycle);
+    			          powerdown();
+    		      }
+    			  if (!(cycle % 360))
+              {
+                  posstatus = false;
+                    homestatus = 1;
+                }
+    			          break;
+          
+          case 3:                           //状态3，电池电量低（锂充电电池电压低于3.5V），灯闪烁6秒 (提示要充电）后，立即关机。
+            for (int i = 0; i<9 ; i++){
+              digitalWrite(kLightPin, HIGH);
+                delay(180);
+                  digitalWrite(kLightPin, LOW);
+                    delay(520);      
+                }
+          		  powerdown();
+          		    break;
+          
+          default:
+    		  	powerdown();
+    			    break;
           }
+    	  delay(1000);
+           }
 
-    	  if (cycle == 1 || !(cycle % 100)) {
-    		  posstatus = false;
-    		  homestatus = 1;
-    	  }	  
-        else {
-    		  EEPROM.put(0x00, cycle);
-    		  powerstandby();
-          }
-    			break;
-      
-      case 1:                  //状态1，自动调整太阳能板位置一次
-  		  setpos();
-  		  posstatus = true;
-  		  homestatus = 0;
-  			break;
-      
-	  case 2:                   //状态2，晚上（光感应值）或不在舒适温度范围（温度感应值）；运行工作30分种后关机。
-		  if (cycle >= 450) {
-			  cycle = 0;
-			  EEPROM.put(0x00, cycle);
-			  powerdown();
-		  }
-			if (!(cycle % 300))
-     {
-          posstatus = false;
-          homestatus = 1;
-     }
-			break;
-      
-    case 3:                                     //状态3， 电池电量低，灯闪烁6秒 (提示要充电）后，马上关机。
-        for (int i = 0; i<9 ; i++){
-          digitalWrite(kLightPin, HIGH);
-          delay(180);
-          digitalWrite(kLightPin, LOW);
-          delay(520);      
-        }
-      		powerdown();
-      		break;
-      
-    default:
-		  	powerdown();
-			break;
+void portinit(){
+  digitalWrite(kLightPin, LOW);
+    digitalWrite(kPowerSavePin, LOW);
+      digitalWrite(kAirConditionPullPin, LOW);   //  高电平，电机顺时针
+        digitalWrite(kAirConditionPushPin, LOW);   //  高电平，电机逆时针
     }
-	delay(1000);
-   }
+
+void eeprominit(){
+      EEPROM.get(0x10, pos);
+        if(pos >90 && pos < 1)
+          {
+            pos = 3;
+              EEPROM.put(0x10, pos);
+          }
+      EEPROM.get(0x00,cycle);
+    }
 
  void getallval()    //获取所有传感器的数值:
   {      
@@ -163,19 +159,14 @@ void loop() {
 	      read_diff = read_light_h - read_light_l;
        }
 
-void portinit(){
-  digitalWrite(kLightPin, LOW);
-    digitalWrite(kPowerSavePin, LOW);
-      digitalWrite(kAirConditionPullPin, LOW);   //  高电平，电机顺时针
-        digitalWrite(kAirConditionPushPin, LOW);   //  高电平，电机逆时针
-    }
+
  void detectsensors(){
 		bool tempstatus = true;
 		    getsensors();
           if(read_light_h < 300 && read_diff < 200){
                 uint8_t lightvalue = read_light_h;
-                  lightvalue = map(lightvalue, 20, 380, 255, 0);
-                    analogWrite(kLightPin, lightvalue);
+                  lightvalue = map(lightvalue, 20, 380, 255, 0);    //把光度值转换PWM输出（0-255）；约光越低PWM值越高!
+                    analogWrite(kLightPin, lightvalue);             
                       homestatus = 2;
                         tempstatus = false; 
                   }
@@ -186,16 +177,16 @@ void portinit(){
                   }
        
             if ( read_temp < 830 ) {
-                  digitalWrite(kAirConditionPushPin, HIGH);
+                  digitalWrite(kAirConditionPushPin, HIGH);              // 当温度高时，控制马达逆时针转动。
                   digitalWrite(kAirConditionPullPin, LOW);
                     homestatus = 2;
             }
             if (read_temp > 858) {
-                  digitalWrite(kAirConditionPullPin, HIGH);
+                  digitalWrite(kAirConditionPullPin, HIGH);             //当温度低时，控制马达顺时针转动。
                   digitalWrite(kAirConditionPushPin, LOW);
                     homestatus = 2;		
             }
-            if(read_temp >= 833 && read_temp <= 855)
+            if(read_temp >= 833 && read_temp <= 855)                   //温度舒适，关闭马达。
             {
                 digitalWrite(kAirConditionPushPin, LOW);
                 digitalWrite(kAirConditionPullPin, LOW);
@@ -208,107 +199,83 @@ void detectpower(){
        if (read_battery < 450 )  homestatus = 3;   	 
 }
 
+void servoselftest(){
+            for (pos = 3; pos <= 90; pos += 1) { 
+                    myservo.write(pos);             
+                       delay(15);                       
+        }
+      for (pos = 90; pos >= 3; pos -= 1) { 
+                myservo.write(pos);              
+                    delay(15);                       
+      }
+}
 void setpos(){
 		uint16_t firstl, firsth ;
 		int diffl, diffh;
-      for (pos = 3; pos <= 90; pos += 1) { 
-              myservo.write(pos);             
-              delay(15);                       
-        }
-      for (pos = 90; pos >= 3; pos -= 1) { 
-          myservo.write(pos);              
-          delay(15);                       
-      }
-        EEPROM.get(0x10, pos);
-          Serial.print("GET-UP--Position--- = ");
-            Serial.println(pos);
-              delay(500);
-                  for (pos; pos <= 90; pos += 1)
-                  { 
-                        myservo.write(pos);
-                          Serial.print("-UP--Position--- = ");
-                          Serial.println(pos);
-                          //delay(300);
-                        delay(1200);
-                        getsensors();
-                          firstl = read_light_l;
-                          firsth = read_light_h;
-                          pos = pos + 2;
+    servoselftest();
+      EEPROM.get(0x10, pos);
+        for (pos; pos <= 90; pos += 1)
+              { 
+                myservo.write(pos);
+                  delay(1200);
+                    getsensors();
+                      firstl = read_light_l;
+                      firsth = read_light_h;
+                        pos = pos + 2;
                           myservo.write(pos);
-                          Serial.print("-UP-NEXT-Position--- = ");
-                          Serial.println(pos);
-                          //  delay(300);
-                        delay(1200);
-                        getsensors();
-                        diffl = read_light_l - firstl;
-                        diffh = read_light_h - firsth;
-                            Serial.print("DIFFL = ");
-                            Serial.println(diffl);
-                            delay(200);
-                            Serial.print("DIFFH = ");
-                            Serial.println(diffh);
-                            delay(200);
-                        if(diffh <= 0 && diffl <= 0)
-                          break;
-                  }
+                            delay(1200);
+                              getsensors();
+                                diffl = read_light_l - firstl;
+                                diffh = read_light_h - firsth;
+                                  if(diffh <= 0 && diffl <= 0)  break;
+                        }
         for (pos; pos >= 3; pos -=1) {
-            myservo.write(pos);
-              delay(1200);
-                getsensors();
-                  firstl = read_light_l;
-                  firsth = read_light_h;     
-                    pos = pos - 2;
-                    myservo.write(pos);
-                      delay(1200);
-                      getsensors();
-                        diffl = read_light_l - firstl;
-                        diffh = read_light_h - firsth;
-                          if(diffh <= 0 && diffl <= 0)  break;
-                  }
-        
-        Serial.print("-----------------------Position--- = ");
-              Serial.println(pos);
-              delay(300);
+              myservo.write(pos);
+                delay(1200);
+                  getsensors();
+                    firstl = read_light_l;
+                    firsth = read_light_h;     
+                      pos = pos - 2;
+                        myservo.write(pos);
+                          delay(1200);
+                            getsensors();
+                              diffl = read_light_l - firstl;
+                              diffh = read_light_h - firsth;
+                                if(diffh <= 0 && diffl <= 0)  break;
+                  }      
+//        Serial.print("---Position--- = ");
+//              Serial.println(pos);
+//              delay(300);
               EEPROM.put(0x10, pos);
     }
 
   void powerdown()   // 控制器省电关机模式.
   {
-  		 // digitalWrite(kPowerSavePin, LOW);
           portinit();
     		  ADCSRA &= ~(1 << ADEN);
     		  MCUCSR |= (1 << JTD);
   
-		// if (period != SLEEP_FOREVER)
-		//   {
-		// 		WDTCR |= (1 << WDCE) | (1 << WDE);
-		// 		WDTCR = (1 << WDP2) | (1 << WDP1) | (1 << WDP0);   // 约2秒看门狗RESET设置
-		// 		WDTCR |= (1 << WDE);
-		//   }
-     
 		      set_sleep_mode(SLEEP_MODE_PWR_DOWN);
 		        cli();
 		          sleep_enable();
   		          sei();
   		            sleep_cpu();   
       		            sleep_disable();
-                        sei();
-                        ADCSRA |= (1 << ADEN);
-  }
+							sei();
+								ADCSRA |= (1 << ADEN);
+	}
 
 void powerstandby(){       // 控制器省电待机模式.
         portinit();
           ADCSRA &= ~(1 << ADEN);
           MCUCSR |= (1 << JTD);  
             WDTCR |= (1 << WDCE) | (1 << WDE);
-            WDTCR = (1 << WDP2) | (1 << WDP1) | (1 << WDP0);   // 约2秒看门狗RESET设置
-            WDTCR |= (1 << WDE);  
+            WDTCR = (1 << WDP2) | (1 << WDP1) | (1 << WDP0);   // 约1.9秒看门狗RESET设置
+            WDTCR |= (1 << WDE);                               //开启看门狗
               set_sleep_mode(SLEEP_MODE_STANDBY);
                  cli();
                   sleep_enable();
-                  sei();
-                    Serial.println("Enter_Cpu_STANDBY_Status: ");
-                    delay(100);
+                    sei();
                     sleep_cpu();       
                       sleep_disable();
                         WDTCR = (1 << WDCE) | (1 << WDE);
